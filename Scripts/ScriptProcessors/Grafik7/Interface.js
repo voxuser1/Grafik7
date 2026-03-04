916439df6478046aa0b0d36ba847fd559dec274f
TomZalud <132501168+voxuser1@users.noreply.github.com>
2026-03-04 15:21:16 +0100

    Add files via upload

---
+++ Scripts/ScriptProcessors/Grafik7/Interface.js
@@ -0,0 +1,23 @@
+Content.makeFrontInterface(620
+, 333
+)function onNoteOn()
+{
+	
+}
+ function onNoteOff()
+{
+	
+}
+ function onController()
+{
+	
+}
+ function onTimer()
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
